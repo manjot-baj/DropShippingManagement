81161dfbcc2fcec337412f99b8aa10fd51c7f215
manjot-baj <bajwa.mj78@gmail.com>
2025-04-21 06:44:06 +0530

    DSM-24 dashboard

--- DripShopPro/apps/dashboard/views.py
+++ DripShopPro/apps/dashboard/views.py
@@ -5,9 +5,11 @@ from user_profile.models import UserProfile
 from user_profile.middlewares import RoleRequiredMixin
 from django.views import View
 from django.contrib import messages
-from store.models import StoreProduct
-from catalog.models import Category, ProductImage
-from order.models import Cart
+from store.models import StoreProduct, Store
+from catalog.models import Category, ProductImage, Inventory
+from order.models import Cart, OrderItem
+from billing.models import PurchaseOrderInvoice
+from common.functions import ORDER_STATUS_COLORS
 
 logger = logging.getLogger("error_log")
 
@@ -18,7 +20,98 @@ class VendorDashboardView(RoleRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         try:
             user = UserProfile.objects.get(user=request.user)
-            return render(request, f"vendor/dashboard.html", {"user": user})
+            approval_pending = PurchaseOrderInvoice.objects.filter(
+                reciever__user=request.user,
+                is_invoiced=False,
+                status="Approval_Pending",
+            ).count()
+            total_orders = OrderItem.objects.filter(
+                vendor__user=request.user,
+                is_deleted=False,
+            ).count()
+            orders_in_progress = OrderItem.objects.filter(
+                vendor__user=request.user,
+                is_deleted=False,
+                status__in=["Confirmed", "Shipped"],
+            ).count()
+            orders_delivered = OrderItem.objects.filter(
+                vendor__user=request.user, is_deleted=False, status="Delivered"
+            ).count()
+
+            payment_due_invoices = PurchaseOrderInvoice.objects.filter(
+                reciever__user=request.user, is_invoiced=True, status="Payment_Pending"
+            )
+            payment_dues = 0
+            for each in payment_due_invoices:
+                order_item = each.order_item
+                vendor_payment_amount = order_item.vendor_price * int(
+                    order_item.quantity
+                )
+                payment_dues = float(payment_dues) + float(vendor_payment_amount)
+
+            invoices = PurchaseOrderInvoice.objects.filter(
+                reciever__user=request.user,
+                is_invoiced=True,
+                status="Payment_Completed",
+            )
+            revenue = 0
+            for each in invoices:
+                order_item = each.order_item
+                vendor_payment_amount = order_item.vendor_price * int(
+                    order_item.quantity
+                )
+                revenue = float(revenue) + float(vendor_payment_amount)
+            catalog_products = (
+                Inventory.objects.select_related("company", "product")
+                .filter(
+                    is_deleted=False,
+                    catalog_display=True,
+                )
+                .count()
+            )
+            catalog_product_list = (
+                Inventory.objects.select_related("company", "product").filter(
+                    is_deleted=False,
+                    catalog_display=True,
+                )
+            )[:5]
+
+            customer_order_data = []
+            customer_orders = OrderItem.objects.filter(
+                vendor__user=request.user, is_deleted=False
+            )[:5]
+
+            for order_item in customer_orders:
+                total_amount = float(order_item.total_amount)
+                customer_order_data.append(
+                    {
+                        "order_date": str(order_item.parent.created_at.date()),
+                        "order_item_id": order_item.pk,
+                        "tracking_id": order_item.tracking_id,
+                        "product": order_item.product.inventory.product.name,
+                        "quantity": int(order_item.quantity),
+                        "total_amount": total_amount,
+                        "store_price": order_item.store_price,
+                        "status": order_item.status,
+                        "status_color": ORDER_STATUS_COLORS[order_item.status],
+                    }
+                )
+
+            data = {
+                "user": user,
+                "total_orders": total_orders,
+                "new_orders": approval_pending,
+                "orders_in_progress": orders_in_progress,
+                "orders_delivered": orders_delivered,
+                "revenue": revenue,
+                "payment_dues": payment_dues,
+                "approval_pending": approval_pending,
+                "catalog_products": catalog_products,
+                "catalog_product_list": catalog_product_list,
+                "customer_order_data": customer_order_data,
+            }
+
+            return render(request, f"vendor/dashboard.html", data)
         except Exception:
             logger.error(traceback.format_exc())
             messages.error(request, "Error fetching companies.")
@@ -36,7 +129,87 @@ class MerchantDashboardView(RoleRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         try:
             user = UserProfile.objects.get(user=request.user)
-            return render(request, f"merchant/dashboard.html", {"user": user})
+            new_orders = OrderItem.objects.filter(
+                merchant__user=request.user, is_deleted=False, status="Placed"
+            ).count()
+            total_orders = OrderItem.objects.filter(
+                merchant__user=request.user,
+                is_deleted=False,
+            ).count()
+            orders_in_progress = OrderItem.objects.filter(
+                merchant__user=request.user,
+                is_deleted=False,
+                status__in=["Confirmed", "Shipped"],
+            ).count()
+            orders_delivered = OrderItem.objects.filter(
+                merchant__user=request.user, is_deleted=False, status="Delivered"
+            ).count()
+            approval_pending = PurchaseOrderInvoice.objects.filter(
+                sender__user=request.user, is_invoiced=False, status="Approval_Pending"
+            ).count()
+            payment_due_invoices = PurchaseOrderInvoice.objects.filter(
+                sender__user=request.user, is_invoiced=True, status="Payment_Pending"
+            )
+            payment_dues = 0
+            for each in payment_due_invoices:
+                order_item = each.order_item
+                vendor_payment_amount = order_item.vendor_price * int(
+                    order_item.quantity
+                )
+                payment_dues = float(payment_dues) + float(vendor_payment_amount)
+
+            invoices = PurchaseOrderInvoice.objects.filter(
+                sender__user=request.user, is_invoiced=True, status="Payment_Completed"
+            )
+            revenue = 0
+            for each in invoices:
+                order_item = each.order_item
+                merchant_profit_amount = order_item.merchant_margin_price * int(
+                    order_item.quantity
+                )
+                revenue = float(revenue) + float(merchant_profit_amount)
+            store_products = StoreProduct.objects.filter(
+                store__owner__user=request.user
+            ).count()
+            store_product_list = StoreProduct.objects.filter(
+                store__owner__user=request.user
+            )[:5]
+
+            customer_order_data = []
+            customer_orders = OrderItem.objects.filter(
+                merchant__user=request.user, is_deleted=False
+            )[:5]
+
+            for order_item in customer_orders:
+                total_amount = float(order_item.total_amount)
+                customer_order_data.append(
+                    {
+                        "order_date": str(order_item.parent.created_at.date()),
+                        "order_item_id": order_item.pk,
+                        "tracking_id": order_item.tracking_id,
+                        "product": order_item.product.inventory.product.name,
+                        "quantity": int(order_item.quantity),
+                        "total_amount": total_amount,
+                        "store_price": order_item.store_price,
+                        "status": order_item.status,
+                        "status_color": ORDER_STATUS_COLORS[order_item.status],
+                    }
+                )
+
+            data = {
+                "user": user,
+                "new_orders": new_orders,
+                "total_orders": total_orders,
+                "orders_in_progress": orders_in_progress,
+                "orders_delivered": orders_delivered,
+                "revenue": revenue,
+                "payment_dues": payment_dues,
+                "approval_pending": approval_pending,
+                "store_products": store_products,
+                "store_product_list": store_product_list,
+                "customer_order_data": customer_order_data,
+            }
+            return render(request, f"merchant/dashboard.html", data)
         except Exception:
             logger.error(traceback.format_exc())
             messages.error(request, "Error fetching companies.")
